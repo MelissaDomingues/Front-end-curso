d0ee36bf1eb7219313a61d48a723fead6734a49f
Melissa Rios Domingues <85204786+MelissaDomingues@users.noreply.github.com>
2024-08-20 13:23:30 -0300

    Gerador de QR Code
    Nesse projeto eu criei um gerador de QR Code usando uma API (QR Code Generator). Fiz o uso de eventos no JS, alterei conteúdos com "innerText", criei e removi classes com "classList", entre outras coisas...

---
+++ 6- JS_QRCode-Generator/js/scripts.js
@@ -0,0 +1,46 @@
+////////Seletores
+
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = document.querySelector("#qr-form input");
+const qrCodeImg = document.querySelector("#qr-code img");
+
+
+//////// Eventos
+
+//gerando qr code
+function generateQrCode () {
+    const qrCodeInputValue = qrCodeInput.value;
+    console.log(qrCodeInputValue);
+
+    if (!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando código..."
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add("active");
+         qrCodeBtn.innerText = "Código criado!"
+    });
+
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+    if (e.code === "Enter"){
+        generateQrCode();
+    }
+});
+
+// Limpar area d qr code
+
+qrCodeInput.addEventListener("keyup", () => {
+    if (!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerText="Gerar QR Code";
+    }
+})
